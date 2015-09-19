3264310d0d02dd04fcc7a6a2dcd9ea09d95c75c4
swps <wendel.schultz@gmail.com>
2015-09-18 23:14:44 -0700

    Spring template project.

---
+++ src/main/java/com/irispr/data/BackendDataServicesApplication.java
@@ -0,0 +1,12 @@
+package com.irispr.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDataServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDataServicesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/irispr/data/BackendDataServicesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.irispr.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BackendDataServicesApplication.class)
+@WebAppConfiguration
+public class BackendDataServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
