c024774230dadf9ab5d491942f6b9d9501fe414a
swps <wendel.schultz@gmail.com>
2015-09-19 03:44:32 -0700

    A bunch of data modeling.  Probably some issues but whatever.  Get over it.

---
+++ src/main/java/com/irispr/data/models/ContactNote.java
@@ -0,0 +1,38 @@
+package com.irispr.data.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+/**
+ * Created by wendel.schultz on 9/19/15.
+ */
+@Entity
+@Table(name = "contact_notes")
+public class ContactNote {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    @Type( type = "org.jadira.usertype.dateandtime.threeten.PersistentZonedDateTime" )
+    private ZonedDateTime lastLogin;
+
+    @Column
+    private String detail;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id")
+    private Contact contact;
+
+}
