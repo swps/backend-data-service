19c285f02ef7a0be3d825a58a42abf9a034b2286
swps <wendel.schultz@gmail.com>
2015-09-19 15:58:04 -0700

    This crap actually works.  With no collections.  We need a collections agency.

--- src/main/java/com/irispr/data/models/CustomerAccount.java
+++ src/main/java/com/irispr/data/models/CustomerAccount.java
@@ -7,6 +7,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -20,8 +22,7 @@ public class CustomerAccount {
     @Id
     @GeneratedValue(generator="system-uuid")
     @GenericGenerator(name="system-uuid", strategy = "uuid")
-    @Type(type = "uuid-char")
-    private UUID id;
+    private String id;
 
     @Column(nullable = false, columnDefinition = "TIMESTAMP")
     @Type( type = "org.jadira.usertype.dateandtime.threeten.PersistentZonedDateTime" )
@@ -41,10 +42,74 @@ public class CustomerAccount {
 
     @Column
     private Boolean active;
+//
+//    @OneToMany(mappedBy = "account")
+//    private Set<User> users = new HashSet<>();
+//
+//    @OneToMany(mappedBy = "account")
+//    private Set<Client> clients = new HashSet<>();
 
-    @OneToMany(mappedBy = "account")
-    private Set<User> users;
+    public String getId() {
+        return id;
+    }
 
-    @OneToMany(mappedBy = "account")
-    private Set<Client> clients;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ZonedDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(ZonedDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public ZonedDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(ZonedDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubdomain() {
+        return subdomain;
+    }
+
+    public void setSubdomain(String subdomain) {
+        this.subdomain = subdomain;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+//
+//    public Set<User> getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(Set<User> users) {
+//        this.users = users;
+//    }
+//
+//    public Set<Client> getClients() {
+//        return clients;
+//    }
+//
+//    public void setClients(Set<Client> clients) {
+//        this.clients = clients;
+//    }
 }
